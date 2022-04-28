b164ddee4cb38d7da34c545cd11230acf3e5c2df
tonoian <tonoianar.t@gmail.com>
2022-04-28 16:20:36 +0400

    add in .gitignore

--- src/new_task/TestMain.java
+++ src/new_task/TestMain.java
@@ -2,6 +2,7 @@ package new_task;
 
 public class TestMain {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("New test again");
     }
 }
+ 
